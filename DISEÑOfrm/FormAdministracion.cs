62489b28756c4832b0339a82f04dd879c01bb03a
Moran Villatoro <erickkmoran663@gmail.com>
2025-10-26 00:30:22 -0600

    a

---
+++ DISEÑOfrm/FormAdministracion.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CajeroAutomatico
+{
+    public partial class FormAdministracion : Form
+    {
+        public FormAdministracion()
+        {
+            InitializeComponent();
+        }
+
+        private void btnManejoClientes_Click(object sender, EventArgs e)
+        {
+            FrmManejoClientes formManejoClientes = new FrmManejoClientes();
+
+            formManejoClientes.Show();
+
+        }
+
+        private void btnListadoClientes_Click(object sender, EventArgs e)
+        {
+            FrmListadoClientes formListadoClientes = new FrmListadoClientes();
+
+            formListadoClientes.Show();
+
+        }
+
+        private void btnGenerarReporte_Click(object sender, EventArgs e)
+        {
+            FrmGenerarReporte formGenerarReporte = new FrmGenerarReporte();
+
+            formGenerarReporte.Show();
+        }
+    }
+}
