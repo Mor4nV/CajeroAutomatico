510894930f0755dea91dce5b2932573bdbda3df3
Moran Villatoro <erickkmoran663@gmail.com>
2025-11-11 07:41:12 -0600

    ol

--- DISEÑOfrm/FrmRecargarEfectivo.Designer.cs
+++ DISEÑOfrm/FrmRecargarEfectivo.Designer.cs
@@ -1,6 +1,6 @@
-﻿namespace CajeroAutomatico.DISEÑOfrm
+﻿namespace CajeroAutomatico.DiseñoFrm
 {
-    partial class FrmRecargarEfectivo
+    partial class FrmCargarEfectivo
     {
         /// <summary>
         /// Required designer variable.
@@ -28,193 +28,31 @@
         /// </summary>
         private void InitializeComponent()
         {
-            grpValidacion = new GroupBox();
-<<<<<<< HEAD
-            txtUsuario = new TextBox();
-            //this.lblFechaReposicion = new Label();
-            lblCantidadReposicion = new Label();
-            btnContinuar = new Button();
-            dateTimePicker1 = new DateTimePicker();
-=======
-            lblFechaReposicion = new Label();
-            txtNombreEmpleado = new TextBox();
-            lblNombreEmpleado = new Label();
-            dtpFechaReposicion = new DateTimePicker();
-            txtMontoReposicion = new TextBox();
-            lblCantidadReposicion = new Label();
-            btnContinuar = new Button();
->>>>>>> ParteAdmin
-            grpValidacion.SuspendLayout();
+            groupBox1 = new GroupBox();
             SuspendLayout();
             // 
-            // grpValidacion
+            // groupBox1
             // 
-            grpValidacion.BackgroundImage = Properties.Resources.Fondo2;
-            grpValidacion.BackgroundImageLayout = ImageLayout.Stretch;
-<<<<<<< HEAD
-            grpValidacion.Controls.Add(dateTimePicker1);
-            grpValidacion.Controls.Add(txtUsuario);
-            //grpValidacion.Controls.Add(this.lblFechaReposicion);
-=======
-            grpValidacion.Controls.Add(lblFechaReposicion);
-            grpValidacion.Controls.Add(txtNombreEmpleado);
-            grpValidacion.Controls.Add(lblNombreEmpleado);
-            grpValidacion.Controls.Add(dtpFechaReposicion);
-            grpValidacion.Controls.Add(txtMontoReposicion);
->>>>>>> ParteAdmin
-            grpValidacion.Controls.Add(lblCantidadReposicion);
-            grpValidacion.Controls.Add(btnContinuar);
-            grpValidacion.Font = new Font("Impact", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            grpValidacion.Location = new Point(237, 219);
-            grpValidacion.Name = "grpValidacion";
-            grpValidacion.Size = new Size(729, 355);
-            grpValidacion.TabIndex = 2;
-            grpValidacion.TabStop = false;
-            grpValidacion.Text = "Validación";
-            grpValidacion.UseWaitCursor = true;
+            groupBox1.Location = new Point(273, 183);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(454, 217);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "groupBox1";
             // 
-<<<<<<< HEAD
-            // txtUsuario
-            // 
-            txtUsuario.Location = new Point(304, 63);
-            txtUsuario.Name = "txtUsuario";
-            txtUsuario.Size = new Size(312, 31);
-            txtUsuario.TabIndex = 3;
-            txtUsuario.UseWaitCursor = true;
-            // 
-            // lblFechaReposicion
-            // 
-            /*
-            this.lblFechaReposicion.AutoSize = true;
-            this.lblFechaReposicion.Location = new Point(30, 121);
-            this.lblFechaReposicion.Name = "lblFechaReposicion";
-            this.lblFechaReposicion.Size = new Size(190, 23);
-            this.lblFechaReposicion.TabIndex = 2;
-            this.lblFechaReposicion.Text = "Fecha de Reposición";
-            this.lblFechaReposicion.UseWaitCursor = true;
-            */
-=======
-            // lblFechaReposicion
-            // 
-            lblFechaReposicion.AutoSize = true;
-            lblFechaReposicion.Location = new Point(30, 178);
-            lblFechaReposicion.Name = "lblFechaReposicion";
-            lblFechaReposicion.Size = new Size(211, 23);
-            lblFechaReposicion.TabIndex = 8;
-            lblFechaReposicion.Text = "Fecha de la Reposición";
-            lblFechaReposicion.UseWaitCursor = true;
-            // 
-            // txtNombreEmpleado
-            // 
-            txtNombreEmpleado.Location = new Point(344, 116);
-            txtNombreEmpleado.Name = "txtNombreEmpleado";
-            txtNombreEmpleado.Size = new Size(312, 31);
-            txtNombreEmpleado.TabIndex = 7;
-            txtNombreEmpleado.UseWaitCursor = true;
-            // 
-            // lblNombreEmpleado
-            // 
-            lblNombreEmpleado.AutoSize = true;
-            lblNombreEmpleado.Location = new Point(30, 119);
-            lblNombreEmpleado.Name = "lblNombreEmpleado";
-            lblNombreEmpleado.Size = new Size(272, 23);
-            lblNombreEmpleado.TabIndex = 6;
-            lblNombreEmpleado.Text = "Nombre del Empleado a Cargo";
-            lblNombreEmpleado.UseWaitCursor = true;
-            // 
-            // dtpFechaReposicion
-            // 
-            dtpFechaReposicion.Location = new Point(344, 172);
-            dtpFechaReposicion.Name = "dtpFechaReposicion";
-            dtpFechaReposicion.Size = new Size(379, 31);
-            dtpFechaReposicion.TabIndex = 5;
-            dtpFechaReposicion.UseWaitCursor = true;
-            // 
-            // txtMontoReposicion
-            // 
-            txtMontoReposicion.Location = new Point(344, 63);
-            txtMontoReposicion.Name = "txtMontoReposicion";
-            txtMontoReposicion.Size = new Size(312, 31);
-            txtMontoReposicion.TabIndex = 3;
-            txtMontoReposicion.UseWaitCursor = true;
->>>>>>> ParteAdmin
-            // 
-            // lblCantidadReposicion
-            // 
-            lblCantidadReposicion.AutoSize = true;
-            lblCantidadReposicion.Location = new Point(30, 66);
-            lblCantidadReposicion.Name = "lblCantidadReposicion";
-            lblCantidadReposicion.Size = new Size(218, 23);
-            lblCantidadReposicion.TabIndex = 1;
-            lblCantidadReposicion.Text = "Cantidad de Reposicion";
-            lblCantidadReposicion.UseWaitCursor = true;
-            // 
-            // btnContinuar
-            // 
-            btnContinuar.BackColor = SystemColors.ControlText;
-            btnContinuar.FlatStyle = FlatStyle.Popup;
-            btnContinuar.Font = new Font("Impact", 15.75F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
-            btnContinuar.ForeColor = SystemColors.ButtonHighlight;
-<<<<<<< HEAD
-            btnContinuar.Location = new Point(280, 209);
-=======
-            btnContinuar.Location = new Point(279, 238);
->>>>>>> ParteAdmin
-            btnContinuar.Name = "btnContinuar";
-            btnContinuar.Size = new Size(156, 66);
-            btnContinuar.TabIndex = 0;
-            btnContinuar.Text = "Continuar";
-            btnContinuar.UseVisualStyleBackColor = false;
-            btnContinuar.UseWaitCursor = true;
-<<<<<<< HEAD
-            // 
-            // dateTimePicker1
-            // 
-            dateTimePicker1.Location = new Point(304, 115);
-            dateTimePicker1.Name = "dateTimePicker1";
-            dateTimePicker1.Size = new Size(312, 31);
-            dateTimePicker1.TabIndex = 5;
-=======
-            btnContinuar.Click += btnContinuar_Click;
->>>>>>> ParteAdmin
-            // 
-            // FrmRecargarEfectivo
+            // FrmCargarEfectivo
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            BackgroundImage = Properties.Resources.RecargarEfectivofondo;
-            BackgroundImageLayout = ImageLayout.Stretch;
-            ClientSize = new Size(1195, 666);
-            Controls.Add(grpValidacion);
-            Name = "FrmRecargarEfectivo";
-            Text = "FrmRecargarEfectivo";
-<<<<<<< HEAD
-=======
-            Load += FrmRecargarEfectivo_Load;
->>>>>>> ParteAdmin
-            grpValidacion.ResumeLayout(false);
-            grpValidacion.PerformLayout();
+            ClientSize = new Size(1075, 582);
+            Controls.Add(groupBox1);
+            Name = "FrmCargarEfectivo";
+            Text = "FrmCargarEfectivo";
             ResumeLayout(false);
         }
 
         #endregion
 
-        private GroupBox grpValidacion;
-<<<<<<< HEAD
-        private TextBox txtContraseña;
-        private TextBox txtUsuario;
-        private Label lblContraseña;
-        private Label lblCantidadReposicion;
-        private Button btnContinuar;
-        private DateTimePicker dateTimePicker1;
-=======
-        private TextBox txtMontoReposicion;
-        private Label lblCantidadReposicion;
-        private Button btnContinuar;
-        private DateTimePicker dtpFechaReposicion;
-        private Label lblFechaReposicion;
-        private TextBox txtNombreEmpleado;
-        private Label lblNombreEmpleado;
->>>>>>> ParteAdmin
+        private GroupBox groupBox1;
     }
 }
--- DISEÑOfrm/FrmRecargarEfectivo.cs
+++ DISEÑOfrm/FrmRecargarEfectivo.cs
@@ -7,78 +7,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-<<<<<<< HEAD
-=======
-using CajeroAutomatico.Manejadores;
-using CajeroAutomatico.Clases;
-using System.IO;
->>>>>>> ParteAdmin
 
-namespace CajeroAutomatico.DISEÑOfrm
+namespace CajeroAutomatico.DiseñoFrm
 {
-    public partial class FrmRecargarEfectivo : Form
+    public partial class FrmCargarEfectivo : Form
     {
-<<<<<<< HEAD
-=======
-        private ManejadorRecargarEfectivo manejadorRecarga = new ManejadorRecargarEfectivo();
-
->>>>>>> ParteAdmin
-        public FrmRecargarEfectivo()
+        public FrmCargarEfectivo()
         {
             InitializeComponent();
         }
-<<<<<<< HEAD
-=======
-
-        private void btnContinuar_Click(object sender, EventArgs e)
-        {
-            try
-            {
-                if (string.IsNullOrWhiteSpace(txtNombreEmpleado.Text))
-                {
-                    MessageBox.Show("El nombre del empleado es obligatorio.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                    return;
-                }
-                if (!decimal.TryParse(txtMontoReposicion.Text, out decimal montoRecarga) || montoRecarga <= 0)
-                {
-                    MessageBox.Show("El monto de recarga debe ser un número válido mayor a cero.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                    return;
-                }
-
-                string nombreEmpleado = txtNombreEmpleado.Text.Trim();
-                DateTime fechaReposicion = dtpFechaReposicion.Value;
-
-                bool agregado = manejadorRecarga.AgregarRecarga(txtNombreEmpleado.Text.Trim(), montoRecarga, fechaReposicion);
-
-                if (agregado)
-                {
-                    MessageBox.Show("Recarga de efectivo registrada exitosamente.", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                    LimpiarCampos();
-                }
-                else
-                {
-                    MessageBox.Show("No se pudo registrar la recarga de efectivo.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                }
-
-            } 
-            catch (Exception ex)
-            {
-                MessageBox.Show("Error al procesar la recarga: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-
-        }
-
-        private void FrmRecargarEfectivo_Load(object sender, EventArgs e)
-        {
-            dtpFechaReposicion.Value = DateTime.Now;
-        }
-        
-        private void LimpiarCampos()
-        {
-            txtNombreEmpleado.Clear();
-            txtMontoReposicion.Clear();
-            dtpFechaReposicion.Value = DateTime.Now;
-        }
->>>>>>> ParteAdmin
     }
 }
